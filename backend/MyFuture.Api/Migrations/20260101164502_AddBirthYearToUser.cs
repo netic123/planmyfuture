798735acf255d73712c5fe2113317dae342c8464
Eren Demir <eren.demir@softronic.se>
2026-01-01 17:45:13 +0100

    Add BirthYear migration to database

---
+++ backend/MyFuture.Api/Migrations/20260101164502_AddBirthYearToUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFuture.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBirthYearToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BirthYear",
+                table: "Users",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthYear",
+                table: "Users");
+        }
+    }
+}
